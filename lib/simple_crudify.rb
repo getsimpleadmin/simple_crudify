381907e7c50b5ed057e4ea5181b0f7b967f83860
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2019-01-20 20:25:31 +0300

    Add dynamic resource crudify

--- lib/simple_crudify.rb
+++ lib/simple_crudify.rb
@@ -4,5 +4,7 @@ require 'simple_crudify/common_methods'
 require 'simple_crudify/crud_actions'
 require 'simple_crudify/crudify'
 
+require 'simple_crudify/crudify_dynamic_resource'
+
 module SimpleCrudify
 end
---
+++ lib/simple_crudify/crudify_dynamic_resource.rb
@@ -0,0 +1,30 @@
+module ResourceController
+  module CrudifyDynamicResource
+    def create
+      @resource = model_klass.new(resource_params)
+      @resource.save
+
+      respond_to do |format|
+        format.js { render layout: false }
+      end
+    end
+
+    def update
+      @resource = model_klass.find(params[:id])
+      @resource.update(resource_params)
+
+      respond_to do |format|
+        format.js { render layout: false }
+      end
+    end
+
+    def destroy
+      @resource = model_klass.find(params[:id])
+      @resource.destroy
+
+      respond_to do |format|
+        format.js { render layout: false }
+      end
+    end
+  end
+end
